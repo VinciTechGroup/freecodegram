b707bdce6163bf9a68d3ab1a93f8b27c5b8cac5e
Paul Richards <info@henryslaptops.com>
2021-11-04 17:28:44 +0000

    Push to Paul

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index($user)
+    {
+        $user = User::findOrFail($user);
+
+        return view('home', [
+            'user' => $user,
+        ]);
+    }
+}
